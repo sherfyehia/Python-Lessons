55476175b7703847f0d47f70d3670a5fa07350dc
sherfyehia <109470170+sherfyehia@users.noreply.github.com>
2022-12-08 15:31:47 +0200

    First lesson

---
+++ lesson 1 frist 17 videos.py
@@ -0,0 +1,107 @@
+# =======================================================Type of print command==================================
+print('======================================================Type of print command==================================')
+print('hello')
+print("sherif yehia")
+print(1997)
+print(5.5)
+print('i was born in 1997')
+print('hello @#$$@')
+print(True)
+print(False)
+# print (true) >> error
+# True and False is variable (poll) should frist letter with appare case
+print('hello "sherif" yehia')
+print("hello 'sherif' yehia")
+# print("hello "sherif" yehia") >> error
+# print('hello 'sherif' yehia') >> error
+print('python', 'sherif yeia', '@#$', 2022, True)
+# ===========================================================exit command==================================
+print('======================================================exit command==================================')
+print('hello')
+print("code will end here regarding to below exit command")
+# exit()
+# exit will end running script
+print(1997)
+print(5.5)
+# ==========================================================print with +==========================================
+print('#==========================================================print with +==========================================')
+print('hello'+' '+'world')
+# ==========================================================variable form==========================================
+print('#==========================================================variable form==========================================')
+#variable = value
+name = 'python'
+NAME = 'hello'
+name2 = 'world'
+_age = 33
+print(name, NAME, name2, _age)
+# (isidentifier) is a function used to verification if 123 is correction to use as variable name , as we now 123 is not correction
+# var = 123
+# print(var.isidentifier())
+# ==========================================================value form==========================================
+print('#==========================================================value form==========================================')
+name3 = 'sherif yehia'
+age = 25
+kilo = 80.5
+print(name, age, kilo)
+# boolean vvvvvv
+ok1 = True
+ok2 = False
+print(ok1, ok2)
+# ==========================================================number of variable in one row==========================================
+print('#==========================================================number of variable in one row==========================================')
+var1, var2, var3 = 'sherif', 'yehia', 'mostafa'
+print(var2)
+# ==========================================================type of variable==========================================
+print('#==========================================================type of variable==========================================')
+a = 'sherif'
+b = "yehia"
+c = 2022
+d = 2.5
+e = True
+f = False
+g = '153skalds@#$'
+
+print(type(a), type(b), type(c), type(d), type(e), type(f), type(g))
+
+# ==========================================================Variables Multiple Assignment==========================================
+print('#==========================================================Variables Multiple Assignment==========================================')
+info = '''
+sherif yehia
+25
+ahmed yehia
+!@#!@#
+'''
+print(info)
+
+# ==========================================================Arithmetic Operators==========================================
+print('#==========================================================Arithmetic Operators==========================================')
+print(4+5)
+print(6-2)
+print(3*3)
+print(20/10)
+print(7/3)
+print(7//3)
+print(21 % 10)
+# hytl3 baqy el qsma 22rab rqm s7e7 yqbl el qsma 3la 10 hwa el 20 w baqy 1
+print(23 % 10)
+# hytl3 baqy el qsma 22rab rqm s7e7 yqbl el qsma 3la 10 hwa el 20 w baqy 3
+print(20/10)
+a1 = 10
+a2 = 20
+print(a1 + a2)
+# ==========================================================Comparison Operators==========================================
+print('#==========================================================Comparison Operators==========================================')
+num1 = 4
+num2 = 4
+print(num1 == num2)
+num3 = 6
+num4 = 4
+print(num3 != num4)
+num5 = 10
+num6 = 7
+print(num5 > num6)
+print(num5 < num6)
+num7 = 10
+num8 = 7
+print(num5 >= num6)
+print(num5 <= num6)
