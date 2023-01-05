c84894d3363dc06b48e649963f484f3e70f88e1f
sherfyehia <109470170+sherfyehia@users.noreply.github.com>
2023-01-05 11:48:53 +0200

    commit new change

---
+++ lesson 2 from (18-).py
@@ -0,0 +1,24 @@
+##############################Escape series(18)############################
+print("##############################Escape series############################")
+print('learn \npython')
+print('\N{copyright sign}SherifElyehia')
+print('\N{registered sign}SherifElyehia')
+print('\N{up down arrow}SherifElyehia')
+print('\N{left right arrow}SherifElyehia')
+print('\'sherifyehia\'')
+print("sherif\a")
+#################################input (19)###############################
+print('############################ input (19)###############################')
+'''
+basic code for input:-
+var=input()
+'''
+#name = input("Enter your name :")
+#age = input("Enter your age :")
+#print("welcome :" + name + "your age :" + age)
+#################################String to Integer , Integer to String (20)###############################
+print('###########################change String to Integer , Integer to String (20)###############################')
+#age2 = input("enter your age: ")
+# print(type(age2))
+#age3 = int(input("enter your age: "))
+# print(type(age3))
