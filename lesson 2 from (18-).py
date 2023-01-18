5b3acb515afdc8d46ce1a2b28e19960d0a158a70
sherfyehia <109470170+sherfyehia@users.noreply.github.com>
2023-01-18 14:31:26 +0200

    commit

--- lesson 2 from (18-).py
+++ lesson 2 from (18-).py
@@ -24,9 +24,9 @@ print('###########################change String to Integer , Integer to String (
 # print(type(age3))
 #################################convert string varible into integer###############################
 print('##################################convert string varible into integer###############################')
-num1 = int(input('Frist number:'))
-num2 = int(input('second number:'))
-print(num1+num2)
+# num1 = int(input('Frist number:'))
+# num2 = int(input('second number:'))
+# print(num1+num2)
 #################################if conditions###############################
 print('############################################if conditions###########################################')
 """
@@ -34,25 +34,55 @@ if condition
 elif condition
 else
 """
-x = 30
-y = 30
-if x > y:
-    print('yes it is x is bigger ')
-elif x < y:
-    print('yes that is true y is bigger')
-elif x == y:
-    print('x is equal to y')
-else:
-    print('try again')
+# x = 30
+# y = 30
+# if x > y:
+#     print('yes it is x is bigger ')
+# elif x < y:
+#     print('yes that is true y is bigger')
+# elif x == y:
+#     print('x is equal to y')
+# else:
+#     print('try again')
 
-a = 10
-b = 5
-if a == 10 and b == 20:
-    print('yes it true')
-else:
-    print('that is wrong')
+# a = 10
+# b = 5
+# if a == 10 and b == 20:
+#     print('yes it true')
+# else:
+#     print('that is wrong')
 
-if a == 10 or b == 20:
-    print('yes it true')
-else:
-    print('that is wrong')
+# if a == 10 or b == 20:
+#     print('yes it true')
+# else:
+#     print('that is wrong')
+###################################example if condition############
+print("####################################example using if condition#############")
+# speed = int(input('how much speed of your car?'))
+# traffic = input('do you break the red light?')
+# if speed > 100:
+#     print('you will go to jail to incresing in speed')
+# elif traffic == 'yes':
+#     print('you will go to jail for break red light')
+# else:
+#     print('thanks for respect')
+###################################using /n /t /a############
+print("####################################using /n /t /a#############")
+print('sherif \nyehia \tmostafa \arezq')
+##################################colors###############################
+print('##################################colors###############################')
+print('\033[1;30;40mSheirf Yehia')
+print('\033[1;31;40mSheirf Yehia')
+print('\033[1;32;40mSheirf Yehia')
+print('\033[1;33;40mSheirf Yehia')
+print('\033[1;34;40mSheirf Yehia')
+print('\033[1;35;40mSheirf Yehia')
+##################################range###############################
+print('##################################range###############################')
+print(range(10))
+print(range(5, 10))
+print(list(range(1, 100)))
+##################################characters###############################
+print('##################################characters###############################')
+for i in range(ord('a'), ord('z')):
+    print(chr(i))
