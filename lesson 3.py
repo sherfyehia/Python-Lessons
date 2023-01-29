a1bac51d89f8b87da30841a3466629937368a8f9
sherfyehia <109470170+sherfyehia@users.noreply.github.com>
2023-01-29 15:47:18 +0200

    done lesson 3

--- lesson 3.py
+++ lesson 3.py
@@ -78,3 +78,106 @@ convert_month = {
 print(convert_month["mar"])
 print(convert_month.get("feb"))
 print(convert_month.get("apr", "there is not value here"))
+
+
+##############################Python While Loop ############################
+print("##############################Python While Loop############################")
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+print("the loop has ended")
+
+
+# while True:
+#     print(i)
+#     i = i + 1
+
+# print("infinite loop")
+
+k = 1
+
+while k <= 10:
+    print(k)
+    k = k + 1
+
+else:
+    print("k become more than 10 so the condition become false")
+
+print("the loop has ended")
+
+f = 1
+
+while f <= 10:
+    f = f + 1
+    if f == 6:
+        continue
+    print(f)
+
+print("we have skip number 6 using continue keyword")
+
+
+b = 1
+
+while b <= 10:
+    b = b + 1
+    if b == 6:
+        break
+    print(b)
+
+print("we have break loop at number 6 using break keyword")
+
+
+##############################Python For Loop ############################
+print("##############################Python For Loop############################")
+
+for letter in "sherifyehia":
+    print(letter)
+
+
+buddies = ['kira', 'light', 'misa', 'ryuk']
+
+for char in buddies:
+    print(char)
+
+
+##############################print with len (number of variable) ############################
+print("##############################print with len (number of variable)############################")
+
+print(len(buddies))
+
+
+for index in range(10):
+    if index % 2 == 0:
+        print(index, "is an even number")
+    else:
+        print(index, "is an odd number")
+
+
+deathnote = ['kira', 'light', 'misa', 'ryuk']
+
+for charaterics in range(len(deathnote)):
+    if deathnote[charaterics] == 'ryuk':
+        print(charaterics, 'is  ryuk')
+    else:
+        print(charaterics, 'in not ryuk')
+
+
+##############################python power ############################
+print("##############################python power############################")
+
+print(2**3)
+
+
+def power(base_num, pow_num):
+    result = 1
+    for index2 in range(pow_num):
+        result = result * base_num
+    return result
+
+
+print(power(2, 3))
