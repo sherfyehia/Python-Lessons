b730e74e8f32b9ef9ab953ff34376c87d5bafdf9
sherfyehia <109470170+sherfyehia@users.noreply.github.com>
2023-02-06 10:50:28 +0200

    cont lesson 4

--- lesson 4.py
+++ lesson 4.py
@@ -35,23 +35,53 @@ print("############################## Python Errors (try)(except) ##############
 ############################## Python Errors (try)(except) ############################
 print("############################## Python Errors (try)(except) ############################")
 
-try:
-    value2 = int(input("Enter a number:"))
-    print(value2)
-    result = 10/0
-except ZeroDivisionError:  # using to handle spacify error
-    print('you cannot divide by zero')
-except ValueError:  # using to handle spacify error
-    print('invalid input')
+# try:
+#     value2 = int(input("Enter a number:"))
+#     print(value2)
+#     result = 10/0
+# except ZeroDivisionError:  # using to handle spacify error
+#     print('you cannot divide by zero')
+# except ValueError:  # using to handle spacify error
+#     print('invalid input')
 
 ############################## Python Errors (try)(except with as) ############################
 print("############################## Python Errors (try)(except with as) ############################")
 
-try:
-    value2 = int(input("Enter a number:"))
-    print(value2)
-    result = 10/0
-except ZeroDivisionError as err:  # using to handle spacify error
-    print(err)
-except ValueError as err2:  # using to handle spacify error
-    print(err2)
+# try:
+#     value2 = int(input("Enter a number:"))
+#     print(value2)
+#     result = 10/0
+# except ZeroDivisionError as err:  # using to handle spacify error
+#     print(err)
+# except ValueError as err2:  # using to handle spacify error
+#     print(err2)
+
+############################## Python Reading Files ############################
+print("############################## Python Reading Files ############################")
+workers_file = open("worker.txt", "r")
+print(workers_file.readable())
+workers_file.close()
+
+workers_file2 = open("worker.txt", "r")
+print(workers_file2.read())
+workers_file2.close()
+
+
+workers_file3 = open("worker.txt", "r")
+print(workers_file3.readline())
+print(workers_file3.readline())
+workers_file3.close()
+
+workers_file4 = open("worker.txt", "r")
+print(workers_file4.readlines())
+workers_file4.close()
+
+
+workers_file5 = open("worker.txt", "r")
+for worker in workers_file5.readlines():
+    print(worker)
+workers_file5.close()
+
+
+############################## Python Writing Files ############################
+print("############################## Python Writing Files ############################")
